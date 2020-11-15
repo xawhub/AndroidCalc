24d4c2ed00f32ef7514af562da551970ce26eb28
xawhub <ksawery.szwankowski@gmail.com>
2020-11-15 04:24:55 +0100

    SimpleCalculator finished

--- app/src/main/java/com/example/myapplication/SimpleCalc.java
+++ app/src/main/java/com/example/myapplication/SimpleCalc.java
@@ -1,22 +1,16 @@
 package com.example.myapplication;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
 import java.math.BigDecimal;
 import java.math.MathContext;
-import java.util.ArrayList;
-import java.util.List;
+
 
 public class SimpleCalc extends AppCompatActivity {
-
-    double input1 = 0, input2 = 0;
 
     private static final int [] buttons_num_ids = {
             R.id._0,
@@ -44,20 +38,27 @@ public class SimpleCalc extends AppCompatActivity {
             R.string._9
     };
 
-    TextView screen;
+    double input1 = 0, input2 = 0;
 
-    boolean Addition, Subtract, Multiplication, Division, empty, decimal, ac_click;
+    private int switcher = 0;
+
+    boolean Addition, Subtract, Multiplication, Division, ac_click, decimal;
+
+    String input;
 
     Button butt_dot, butt_c, butt_ac, butt_add, butt_sub, butt_mul, butt_div, butt_equ, butt_opp;
+
+    TextView screen;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simple_calc);
 
-        screen = (TextView)findViewById(R.id.calc_value);
+        screen = (TextView) findViewById(R.id.calc_value);
 
-        butt_dot = (Button)findViewById(R.id.dot);
+        butt_dot = (Button) findViewById(R.id.dot);
         butt_c = (Button)findViewById(R.id.c);
         butt_ac = (Button)findViewById(R.id.ac);
         butt_add = (Button)findViewById(R.id.add);
@@ -78,100 +79,128 @@ public class SimpleCalc extends AppCompatActivity {
                 button.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        screen.setText(screen.getText() + finalButt_num);
+                        screen.setText(parseOutput(Double.parseDouble((String) screen.getText() + finalButt_num)));
                     }});
         }
 
         butt_dot.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-              //  String screenTmp = (String) screen.getText();
-                if (decimal) {
-                    //do nothing or you can show the error
-                } else /*if (!screenTmp.contains("."))*/ {
-                    if(screen.getText().length() == 0){
-                        screen.setText(screen.getText() + "0.");
+                String screenTmp = (String) screen.getText();
+                if (!screenTmp.contains(".")) {
+                    if (screen.getText().length() == 0) {
+                        screen.setText(String.format("%s0.", screen.getText()));
                     } else {
-                        screen.setText(screen.getText() + ".");
+                        screen.setText(String.format("%s.", screen.getText()));
                     }
-                    decimal = true;
                 }
             }
         });
 
-        butt_c.setOnClickListener(new View.OnClickListener() {
+        butt_c.setOnClickListener(new DoubleClickListener() {
+            @SuppressLint("UseCompatLoadingForDrawables")
             @Override
-            public void onClick(View v) {
-                if (!ac_click){
-                    input2 = 0.0;
-                    butt_c.setText(R.string.ec);
-                    ac_click = !ac_click;
-                }else {
-                    input1 = 0.0;
-                    input2 = 0.0;
-                    butt_c.setText(R.string.c);
-                    ac_click = !ac_click;
-                }
-                decimal = !decimal;
+            public void onDoubleClick() {
+                input1 = 0.0;
+                input2 = 0.0;
+                screen.setText("");
+                butt_add.setBackground(getResources().getDrawable(R.drawable.custom_button));
+                butt_sub.setBackground(getResources().getDrawable(R.drawable.custom_button));
+                butt_div.setBackground(getResources().getDrawable(R.drawable.custom_button));
+                butt_mul.setBackground(getResources().getDrawable(R.drawable.custom_button));
+            }
+
+            @Override
+            public void onSingleClick() {
+                input2 = 0.0;
                 screen.setText("");
             }
         });
 
         butt_ac.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("UseCompatLoadingForDrawables")
             @Override
             public void onClick(View v) {
-                decimal = !decimal;
-                screen.setText("");
-                input1 = 0.0;
-                input2 = 0.0;
+                butt_add.setBackground(getResources().getDrawable(R.drawable.custom_button));
+                butt_sub.setBackground(getResources().getDrawable(R.drawable.custom_button));
+                butt_div.setBackground(getResources().getDrawable(R.drawable.custom_button));
+                butt_mul.setBackground(getResources().getDrawable(R.drawable.custom_button));
+                if (screen.getText() != "") {
+                    //decimal = !decimal;
+                    screen.setText("");
+                    input1 = 0.0;
+                    input2 = 0.0;
+                }
             }
         });
 
         butt_add.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("UseCompatLoadingForDrawables")
             @Override
             public void onClick(View v) {
+                Add();
+                butt_add.setBackground(getResources().getDrawable(R.drawable.custom_button_active));
+                butt_sub.setBackground(getResources().getDrawable(R.drawable.custom_button));
+                butt_div.setBackground(getResources().getDrawable(R.drawable.custom_button));
+                butt_mul.setBackground(getResources().getDrawable(R.drawable.custom_button));
                 if (screen.getText().length() != 0) {
-
-                        input1 = Double.parseDouble(screen.getText() + "");
-                        Addition = true;
-                        decimal = false;
-                        screen.setText(null);
-                    }
+                    switcher = 1;
+                    input1 = Double.parseDouble((String) screen.getText());
+                    // decimal = false;
+                    screen.setText(null);
                 }
-
+            }
         });
 
         butt_sub.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("UseCompatLoadingForDrawables")
             @Override
             public void onClick(View v) {
+                Sub();
+                butt_add.setBackground(getResources().getDrawable(R.drawable.custom_button));
+                butt_sub.setBackground(getResources().getDrawable(R.drawable.custom_button_active));
+                butt_div.setBackground(getResources().getDrawable(R.drawable.custom_button));
+                butt_mul.setBackground(getResources().getDrawable(R.drawable.custom_button));
                 if (screen.getText().length() != 0) {
+                    switcher = 1;
                     input1 = Double.parseDouble((String) screen.getText());
-                    Subtract = true;
-                    decimal = false;
+                    //   decimal = false;
                     screen.setText(null);
                 }
             }
         });
 
         butt_mul.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("UseCompatLoadingForDrawables")
             @Override
             public void onClick(View v) {
+                Mul();
+                butt_add.setBackground(getResources().getDrawable(R.drawable.custom_button));
+                butt_sub.setBackground(getResources().getDrawable(R.drawable.custom_button));
+                butt_div.setBackground(getResources().getDrawable(R.drawable.custom_button));
+                butt_mul.setBackground(getResources().getDrawable(R.drawable.custom_button_active));
                 if (screen.getText().length() != 0) {
-                    input1 = Double.parseDouble((String)screen.getText());
-                    Multiplication = true;
-                    decimal = false;
+                    switcher = 1;
+                    input1 = Double.parseDouble((String) screen.getText());
+                    //    decimal = false;
                     screen.setText(null);
                 }
             }
         });
 
         butt_div.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("UseCompatLoadingForDrawables")
             @Override
             public void onClick(View v) {
+                Div();
+                butt_add.setBackground(getResources().getDrawable(R.drawable.custom_button));
+                butt_sub.setBackground(getResources().getDrawable(R.drawable.custom_button));
+                butt_div.setBackground(getResources().getDrawable(R.drawable.custom_button_active));
+                butt_mul.setBackground(getResources().getDrawable(R.drawable.custom_button));
                 if (screen.getText().length() != 0) {
-                    input1 = Double.parseDouble(screen.getText() + "");
-                    Division = true;
-                    decimal = false;
+                    switcher = 1;
+                    input1 = Double.parseDouble((String) screen.getText());
+                    //    decimal = false;
                     screen.setText(null);
                 }
             }
@@ -180,6 +209,7 @@ public class SimpleCalc extends AppCompatActivity {
         butt_equ.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // if (screen.getText() != ""){ equal();}
                 equal();
             }
         });
@@ -187,10 +217,17 @@ public class SimpleCalc extends AppCompatActivity {
         butt_opp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                System.out.println(switcher);
                 if (screen.getText().length() != 0 && Double.parseDouble((String)screen.getText()) != 0) {
-                    input1 = Double.parseDouble(screen.getText() + "");
-                    input1 = -input1;
-                    screen.setText(parseOutput(input1));
+                    if (switcher == 0) {
+                        input1 = Double.parseDouble((String) screen.getText());
+                        input1 = -input1;
+                        screen.setText(parseOutput(input1));
+                    } else if (switcher == 1) {
+                        input2 = Double.parseDouble((String) screen.getText());
+                        input2 = -input2;
+                        screen.setText(parseOutput(input2));
+                    }
                 }
             }
         });
@@ -208,52 +245,97 @@ public class SimpleCalc extends AppCompatActivity {
     }
 
 
+    @SuppressLint("UseCompatLoadingForDrawables")
     private void equal() {
-        if (Addition || Subtract || Multiplication || Division) {
-            input2 = Double.parseDouble(screen.getText() + "");
-        }
-
-        System.out.println(input1);
-        System.out.println(input2);
-            BigDecimal bdi1 = new BigDecimal(input1);
-            BigDecimal bdi2 = new BigDecimal(input2);
-
-        if (Addition) {
-            screen.setText(bdi1.add(bdi2) + "");
-            Addition=false;
-            Subtract=false;
-            Multiplication=false;
-            Division = false;
-        }
-
-        if (Subtract) {
-            screen.setText(bdi1.subtract(bdi2) + "");
-            Addition=false;
-            Subtract=false;
-            Multiplication=false;
-            Division = false;
-        }
-
-        if (Multiplication) {
-            BigDecimal result = bdi1.multiply(bdi2, MathContext.DECIMAL32);
-            screen.setText(parseOutput(result.doubleValue()));
-            Addition=false;
-            Subtract=false;
-            Multiplication=false;
-            Division = false;
-        }
-
-        if (Division) {
-            if(input2 == 0.0) {
-                screen.setText("");
-            } else {
-                BigDecimal result = bdi1.divide(bdi2, MathContext.DECIMAL32);
-                screen.setText(parseOutput(result.doubleValue()));
+        switcher = 0;
+        BigDecimal bdi1;
+        BigDecimal bdi2;
+        if (screen.getText() != "") {
+            if (Addition || Subtract || Multiplication || Division) {
+                input2 = Double.parseDouble((String) screen.getText());
             }
-            Addition=false;
-            Subtract=false;
-            Multiplication=false;
-            Division = false;
+            bdi1 = new BigDecimal(input1);
+            bdi2 = new BigDecimal(input2);
+
+            if (Addition) {
+                BigDecimal result = bdi1.add(bdi2, MathContext.DECIMAL32);
+                screen.setText(parseOutput(result.doubleValue()));
+                reset();
+            }
+
+            if (Subtract) {
+                BigDecimal result = bdi1.subtract(bdi2, MathContext.DECIMAL32);
+                screen.setText(parseOutput(result.doubleValue()));
+                reset();
+            }
+
+            if (Multiplication) {
+                BigDecimal result = bdi1.multiply(bdi2, MathContext.DECIMAL32);
+                screen.setText(parseOutput(result.doubleValue()));
+                reset();
+            }
+
+            if (Division) {
+                if (input2 == 0.0) {
+                    screen.setText("");
+                } else {
+                    BigDecimal result = bdi1.divide(bdi2, MathContext.DECIMAL32);
+                    screen.setText(parseOutput(result.doubleValue()));
+                }
+                reset();
+            }
+            butt_add.setBackground(getResources().getDrawable(R.drawable.custom_button));
+            butt_sub.setBackground(getResources().getDrawable(R.drawable.custom_button));
+            butt_div.setBackground(getResources().getDrawable(R.drawable.custom_button));
+            butt_mul.setBackground(getResources().getDrawable(R.drawable.custom_button));
         }
+    }
+
+    private void reset() {
+        Addition = false;
+        Subtract = false;
+        Multiplication = false;
+        Division = false;
+    }
+
+    private void Add() {
+        Addition = true;
+        Subtract = false;
+        Multiplication = false;
+        Division = false;
+    }
+
+    private void Sub() {
+        Addition = false;
+        Subtract = true;
+        Multiplication = false;
+        Division = false;
+    }
+
+    private void Mul() {
+        Addition = false;
+        Subtract = false;
+        Multiplication = true;
+        Division = false;
+    }
+
+    private void Div() {
+        Addition = false;
+        Subtract = false;
+        Multiplication = false;
+        Division = true;
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("input", screen.getText().toString());
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        input = savedInstanceState.getString("input");
+        screen.setText(input);
     }
 }
